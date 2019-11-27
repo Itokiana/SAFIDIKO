aa101655807a94ffa984cd999e62cdf24e82b8cd
Itokiana <itokianarajoelison@gmail.com>
2019-11-28 02:26:10 +0300

    Front prod

---
+++ precache-manifest.b504ee41eb9e47de5d8a467a0edbfaea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86b084ef7eee0be09a0aaaec542e138d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a93ff8de57a9233e092c",
+    "url": "/static/css/2.81cb803e.chunk.css"
+  },
+  {
+    "revision": "369eaf58d09e8be088e0",
+    "url": "/static/css/main.ab377b82.chunk.css"
+  },
+  {
+    "revision": "a93ff8de57a9233e092c",
+    "url": "/static/js/2.51c4b6c4.chunk.js"
+  },
+  {
+    "revision": "369eaf58d09e8be088e0",
+    "url": "/static/js/main.0304139a.chunk.js"
+  },
+  {
+    "revision": "32f1743231ad73a3864b",
+    "url": "/static/js/runtime~main.686ddca1.js"
+  }
+]);
