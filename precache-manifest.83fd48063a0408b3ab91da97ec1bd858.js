9adf844a6f19b03f5568e738763ab7fa1edbeaf5
Itokiana <itokianarajoelison@gmail.com>
2019-11-28 02:43:21 +0300

    Updates

---
+++ precache-manifest.83fd48063a0408b3ab91da97ec1bd858.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "759ac238fbc59612c88a6da69d293f98",
+    "url": "/SAFIDIKO/index.html"
+  },
+  {
+    "revision": "73f97e3234c0b799a95a",
+    "url": "/SAFIDIKO/static/css/2.81cb803e.chunk.css"
+  },
+  {
+    "revision": "1d71d22e675380614fd7",
+    "url": "/SAFIDIKO/static/css/main.ab377b82.chunk.css"
+  },
+  {
+    "revision": "73f97e3234c0b799a95a",
+    "url": "/SAFIDIKO/static/js/2.8aeb4f1f.chunk.js"
+  },
+  {
+    "revision": "1d71d22e675380614fd7",
+    "url": "/SAFIDIKO/static/js/main.98505f2d.chunk.js"
+  },
+  {
+    "revision": "6008c2edc7f75b01cbeb",
+    "url": "/SAFIDIKO/static/js/runtime~main.d2080466.js"
+  }
+]);
